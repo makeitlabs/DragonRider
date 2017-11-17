469d333ae3f28fcd40e0baad739229c11cb028b4
Denis Perchenko <plachenkO@gmail.com>
2017-11-16 20:25:13 -0500

    Initial commit

---
+++ Source/DragonRider/DragonRiderGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DragonRiderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAGONRIDER_API ADragonRiderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
