34c28f7591bc1615cf3a7cd0f10e1d58abd39ee8
Denis Perchenko <rndpln@gmail.com>
2017-11-30 21:06:14 -0500

    added dragon and flying capabilities

--- Source/DragonRider/DragonRiderGameModeBase.cpp
+++ Source/DragonRider/DragonRiderGameModeBase.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "DragonRiderGameModeBase.h"
+#include "DragonRiderActor.h"
 
-
-
-
+ADragonRiderGameModeBase::ADragonRiderGameModeBase()
+{
+	// set default pawn class to our flying pawn
+	DefaultPawnClass = ADragonRiderActor::StaticClass();
+}
